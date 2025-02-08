3f3ce1361236539616cba4a7c63dc7c60d12fba9
Robert Manders <github.powdered589@passmail.net>
2025-02-08 18:57:51 +0000

    Update Next JS version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import styles        from './page.module.css'
 import { SearchBar } from '@/components/elements'
 
 export default function Home() {
+	console.log('Example breakpoint...')
+
 	return (
 		<main className={styles.main}>
 			<SearchBar />
